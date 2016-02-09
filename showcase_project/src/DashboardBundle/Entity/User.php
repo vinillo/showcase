5055eb0a22ac44ecfdfefcddad58f77dabaaee6c
Vin <info@webkleur.com>
2016-02-09 12:52:35 +0100

    getter & setters User enity

--- showcase_project/src/DashboardBundle/Entity/User.php
+++ showcase_project/src/DashboardBundle/Entity/User.php
@@ -11,8 +11,19 @@ class User
      * @var int
      */
     private $id;
+    private $username;
+    private $hash;
+    private $email;
+    private $activated;
+    private $avatar_id;
 
-
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
     /**
      * Get id
      *
@@ -22,5 +33,87 @@ class User
     {
         return $this->id;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    /**
+     * @param mixed $hash
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActivated()
+    {
+        return $this->activated;
+    }
+
+    /**
+     * @param mixed $activated
+     */
+    public function setActivated($activated)
+    {
+        $this->activated = $activated;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAvatarId()
+    {
+        return $this->avatar_id;
+    }
+
+    /**
+     * @param mixed $avatar_id
+     */
+    public function setAvatarId($avatar_id)
+    {
+        $this->avatar_id = $avatar_id;
+    }
+
+
 }
 
