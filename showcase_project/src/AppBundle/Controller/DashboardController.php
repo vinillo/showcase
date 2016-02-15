beff6930f585c4113ea419d76db75f5a6d6e5905
Vin <info@webkleur.com>
2016-02-15 17:21:02 +0100

    fb avatar/public profile picture handeling. make fb user account when not exist in User entity.

--- showcase_project/src/AppBundle/Controller/DashboardController.php
+++ showcase_project/src/AppBundle/Controller/DashboardController.php
@@ -74,6 +74,17 @@ class DashboardController extends Controller
 
         $result = $query->getResult();
         $comment_data = $result;
+        if($this->session->get("fb_login") == true):
+            $fb_bool = true;
+        else:
+            $fb_bool = "";
+        endif;
+        if($this->session->get("fb_realm_name")):
+            $fb_name = $this->session->get("fb_realm_name");
+        else:
+            $fb_name  = $this->session->get("email");
+        endif;
+
 
         if ($request->getMethod() == 'POST' && $request->get('comment_submit')):
             $comment = new comment();
@@ -94,7 +105,7 @@ class DashboardController extends Controller
         else:
             $avatar_data_img = "";
         endif;
-        if(isset($action) && isset($id)):
+        if (isset($action) && isset($id)):
             //action=get_app&id=' . $id
             $app_info = file_get_contents('http://localhost/hello/itsme.php');
             $app_info = json_decode($app_info, true);
@@ -105,6 +116,10 @@ class DashboardController extends Controller
                     'user_id' => $this->session->get("user_id"),
                     'avatar_src' => $this->session->get("avatar_src"),
                     'avatar_data_img' => $avatar_data_img,
+                    'email' => $this->session->get("email"),
+                    'created' => $this->session->get("created"),
+                    'fb_bool' => $fb_bool,
+                    'fb_real_name' => $fb_name,
                     'comment_data' => $comment_data,
                     'comment_exist' => $no_comments,
                     'tag_title' => $app_info['title'],
@@ -114,19 +129,23 @@ class DashboardController extends Controller
             );
             return new Response($html);
         else:
-        $html = $this->container->get('templating')->render(
-            'dashboard/account.html.twig',
-            array(
-                'username' => $this->session->get("username"),
-                'user_id' => $this->session->get("user_id"),
-                'avatar_src' => $this->session->get("avatar_src"),
-                'avatar_data_img' => $avatar_data_img,
-                'comment_data' => $comment_data,
-                'comment_exist' => $no_comments
-            )
-        );
-        return new Response($html);
-                endif;
+            $html = $this->container->get('templating')->render(
+                'dashboard/account.html.twig',
+                array(
+                    'username' => $this->session->get("username"),
+                    'user_id' => $this->session->get("user_id"),
+                    'avatar_src' => $this->session->get("avatar_src"),
+                    'email' => $this->session->get("email"),
+                    'created' => $this->session->get("created"),
+                    'fb_bool' => $fb_bool,
+                    'fb_real_name' => $fb_name,
+                    'avatar_data_img' => $avatar_data_img,
+                    'comment_data' => $comment_data,
+                    'comment_exist' => $no_comments
+                )
+            );
+            return new Response($html);
+        endif;
     }
 
     /**
@@ -172,8 +191,9 @@ class DashboardController extends Controller
                     $user->setUsername($request->request->get('username_register'));
                     $user->setHash($this->makeHash($request->request->get('username_register'), $request->request->get('password_register')));
                     $user->setEmail($request->request->get('email_register'));
-                    $user->setActivated(0);
+                    $user->setActivated(1);
                     $user->setAvatarSrc($request->request->get('avatar_src'));
+                    $user->setCreated(time());
                     $em = $this->getDoctrine()->getManager();
                     $em->persist($user);
                     $em->flush();
@@ -199,12 +219,14 @@ class DashboardController extends Controller
             else:
 
                 $this->session->set('username', $request->request->get('username'));
-
-                $user_id = $this->getUserIdFor($this->session->get('username'));
+                $user_id = $this->getUserResultFor($this->session->get('username'))[0]['id'];
                 $this->session->set('user_id', $user_id['id']);
-
-                $avatar_src = $this->getAvatarSrcFor($this->session->get('user_id'));
-                $this->session->set('avatar_src', $avatar_src['avatar_src']);
+                $avatar_src = $this->getUserResultFor($this->session->get('username'))[0]['avatar_src'];
+                $this->session->set('avatar_src', $avatar_src);
+                $email = $this->getUserResultFor($this->session->get('username'))[0]['email'];
+                $this->session->set('email',  $email);
+                $created = $this->getUserResultFor($this->session->get('username'))[0]['created'];
+                $this->session->set('created', $created);
 
 
                 return $this->redirectToRoute('app_dashboard_account');
@@ -246,32 +268,28 @@ class DashboardController extends Controller
         $query = $repository->createQueryBuilder('t')
             ->select('count(t.id)')
             ->getQuery();
-
         $result = $query->getSingleScalarResult();
         return $result;
     }
 
-    public function getUserIdFor($username)
+
+    public function getUserResultFor($username)
     {
+
         $repository = $this->getDoctrine()->getRepository('AppBundle:User');
         $query = $repository->createQueryBuilder('p')
-            ->select('p.id')
+            ->select('p.id,p.username,p.hash,p.email,p.activated,p.avatar_src,p.created')
             ->where('p.username = :username')
-            ->setParameter('username', $username)->getQuery();
-        return $query->getSingleResult();
+            ->setParameters(['username' => $username])
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(1)
+            ->getQuery();
+
+        return $query->getResult();
 
     }
 
-    public function getAvatarSrcFor($user_id)
-    {
-        $repository = $this->getDoctrine()->getRepository('AppBundle:User');
-        $query = $repository->createQueryBuilder('p')
-            ->select('p.avatar_src')
-            ->where('p.id = :id')
-            ->setParameter('id', $user_id)->getQuery();
-        return $query->getSingleResult();
 
-    }
     /**
      * @Route("/logincallback")
      */
@@ -298,7 +316,7 @@ class DashboardController extends Controller
         if (isset($accessToken)) {
             $fb->setDefaultAccessToken($accessToken);
             try {
-                $requestProfile = $fb->get("/me?fields=name,email");
+                $requestProfile = $fb->get("/me?fields=id,name,email");
                 $profile = $requestProfile->getGraphNode()->asArray();
             } catch (FacebookSDKException $e) {
                 // When Graph returns an error
@@ -308,12 +326,54 @@ class DashboardController extends Controller
                 echo 'Facebook SDK returned an error: ' . $e->getMessage();
             }
             $this->session->set('username', $profile['name']);
-            $this->session->set('avatar_src', "anonymous.png");
+            $this->session->set('email', $profile['email']);
+            if (!$this->checkFbIdExistinUserFor($profile['id'].'_fb')):
+                $user = new User();
+                $user->setUsername($profile['id'] . "_fb");
+                $user->setHash($this->makeHash($profile['name'], $profile['email']));
+                $user->setEmail($profile['email']);
+                $user->setActivated(1);
+                $user->setAvatarSrc($profile['id']);
+                $user->setCreated(time());
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+            endif;
+
+            $fb_user_id = $this->getUserResultFor($profile['id'] . "_fb")[0]['id'];
+            $fb_username = $this->getUserResultFor($profile['id'] . "_fb")[0]['username'];
+            $fb_created = $this->getUserResultFor($profile['id'] . "_fb")[0]['created'];
+            $fb_avatar_src = $this->getUserResultFor($profile['id'] . "_fb")[0]['avatar_src'];
+            $fb_email = $this->getUserResultFor($profile['id'] . "_fb")[0]['email'];
+
+            $this->session->set('username', $fb_username);
+            $this->session->set('fb_realm_name', $profile['name']);
+            $this->session->set('user_id', $fb_user_id);
+            $this->session->set('avatar_src',  $fb_avatar_src);
+            $this->session->set('email',  $fb_email);
+            $this->session->set('created', $fb_created);
+            $this->session->set('fb_login', true);
             return $this->redirectToRoute('app_dashboard_account');
-            exit;
         } else {
             echo "Unauthorized access!!!";
             exit;
         }
+    }
+
+    public function checkFbIdExistinUserFor($fbid)
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:User');
+        $query = $repository->createQueryBuilder('t')
+            ->select('count(t.username)')
+            ->where('t.username = :username')
+            ->setParameters(['username' => $fbid])
+            ->setMaxResults(1)
+            ->getQuery();
+        $result = $query->getSingleScalarResult();
+        if ($result >= 1):
+            return true;
+        else:
+            return false; //no email found make account
+        endif;
     }
 }
