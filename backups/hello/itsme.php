53fe8fe5fe92990d1171b6a21280fb547e2e7a6b
Vincent Provo <info@webkleur.com>
2016-02-24 13:42:02 +0100

    Update itsme.php

--- backups/hello/itsme.php
+++ backups/hello/itsme.php
@@ -1,5 +1,7 @@
 <?php
-$link = mysqli_connect("localhost","root","kippen7","rest_api");
+//THIS FILE IS JUST FOR EXAMPLE -- YES IT IS MYSQL INJECTABLE
+//COMPLETELY UNSAFE AND HORRIBLE STYLE OF CODING -- JUST FOR DEMO PURPOSES AND NEVER WILL BE USED ON A LIVE APP
+$link = mysqli_connect("localhost","root","root","rest_api");
 if(isset($_GET['update']) && isset($_GET['title']) && isset($_GET['body']) && isset($_GET['created_by'])):
 
     mysqli_query($link,"UPDATE message set title = '".$_GET['title']."' , body = '".$_GET['body']."', created_by ='".$_GET['created_by']."' LIMIT 1");
